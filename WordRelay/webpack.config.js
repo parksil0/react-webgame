dde8ad1ac23a49adb1bb8b2f55c9c5e7c5cabefe
parksil0 <parksil00@naver.com>
2021-07-09 00:48:15 +0900

    웹팩 컨피그 초기 설정

--- WordRelay/webpack.config.js
+++ WordRelay/webpack.config.js
@@ -1,3 +1,24 @@
-module.exports = {
-  
+import path from 'path';
+
+export default {
+  name: 'word-relay-setting',
+  mode: 'development', // 실서비스 : production
+  devtool: 'eval',
+
+  // 확장자 연결 도우미
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  // 중요!! - 입력
+  entry: {
+    app: ['./client'],
+  },
+
+  // 중요!! - 출력
+  output: {
+    path: path.join(__dirname, 'dist'), // WordRelay/dist
+    filename: 'app.js'
+  },
+
 }
