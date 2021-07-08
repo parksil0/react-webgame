3dd3c693ad7d318eb57e8bded65cbb770a65267c
parksil0 <parksil00@naver.com>
2021-07-09 00:47:28 +0900

    기본 설정

--- WordRelay/client.jsx
+++ WordRelay/client.jsx
@@ -1,2 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+
+import WordRelay from './wordRelay.jsx';
+
+ReactDom.render(<GuGuDan />, document.querySelector("#root"))
---
+++ WordRelay/wordRelay.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class WordRelay extends React.Component {
+  state = {
+
+  }
+  render() {
+    
+  }
+}
+
+export default { 
+  WordRelay
+}
