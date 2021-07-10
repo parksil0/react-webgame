15e8ab508a173ed7fb64a3e3d63065d206e4f8d5
parksil0 <parksil00@naver.com>
2021-07-11 00:37:52 +0900

    클래스 -> 훅스로 파일 변경

--- WordRelay/client.jsx
+++ WordRelay/client.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import WordRelay from './wordRelay.jsx';
+import WordRelay from './wordRelayHooks.jsx';
 
 ReactDom.render(<WordRelay />, document.querySelector("#root"))
