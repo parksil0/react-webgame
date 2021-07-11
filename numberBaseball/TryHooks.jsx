88aa172d4ac7f24ab03c5cca50104db6b994332d
parksil0 <parksil00@naver.com>
2021-07-11 23:41:27 +0900

    memo 추가

--- numberBaseball/TryHooks.jsx
+++ numberBaseball/TryHooks.jsx
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { memo } from 'react';
 
-const Try = ({ tryInfo }) => {
+const Try = memo(({ tryInfo }) => {
   return (
     <li>
       <b>{tryInfo.try}</b> - {tryInfo.result}
     </li>
   )
-}
+})
 
 export default Try;
