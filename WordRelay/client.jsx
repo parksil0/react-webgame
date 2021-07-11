d67a5ef387d162d6d8b6c3af67c09a487359031b
parksil0 <parksil00@naver.com>
2021-07-11 16:04:18 +0900

    파일 이름 변경

--- WordRelay/client.jsx
+++ WordRelay/client.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import WordRelay from './wordRelayHooks.jsx';
+import WordRelay from './WordRelayHooks.jsx';
 
 ReactDom.render(<WordRelay />, document.querySelector("#root"))
