389b1b2444748123049d54da0b4972fb3665aa1b
parksil0 <parksil00@naver.com>
2021-07-11 23:41:19 +0900

    PureComponent 추가

--- numberBaseball/Try.jsx
+++ numberBaseball/Try.jsx
@@ -1,14 +1,25 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
-class Try extends Component {
-  render () {
+class Try extends PureComponent {
+  render() {
+    const { tryInfo } = this.props;
     return (
-      // 리액트는 key를 보고 같은 컴포넌트인지 아닌지 여부를 확인한다.
       <li>
-        <b>{this.props.tryInfo.try}</b> - {this.props.tryInfo.result}
+        <b>{tryInfo.try}</b> - {tryInfo.result}
       </li>
     )
   }
 }
+
+// class Try extends Component {
+//   render () {
+//     return (
+//       // 리액트는 key를 보고 같은 컴포넌트인지 아닌지 여부를 확인한다.
+//       <li>
+//         <b>{this.props.tryInfo.try}</b> - {this.props.tryInfo.result}
+//       </li>
+//     )
+//   }
+// }
 
 export default Try;
