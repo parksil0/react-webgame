85f54b06720ea80effabf4d42300d08c394a12e7
parksil0 <parksil00@naver.com>
2021-07-17 11:04:41 +0900

    훅스 코드 생성

---
+++ responseCheck/ResponseCheckHooks.jsx
@@ -0,0 +1,72 @@
+import React, { useRef, useState } from "react";
+
+const ResponseCheck = () => {
+  const [state, setState] = useState("waiting");
+  const [message, setMessage] = useState('클릭해서 시작하세요.');
+  const [result, setResult] = useState([]);
+  
+  // hooks에서는 클래스의 this 접근을 ref로 표현한다.
+  // 클래스와 마찬가지로 useRef()를 사용하면 렌더링이 되지 않는다.
+  const timeout = useRef(null);
+  const startTime = useRef();
+  const endTime = useRef();
+
+  const onClickScreen = () => {
+    if (state === "waiting") {
+      setState("ready");
+      setMessage("초록색이 되면 클릭하세요.");
+      timeout.current = setTimeout(() => {
+        setState('now');
+        setMessage('지금 클릭'),
+        startTime.current = new Date();
+      }, Math.floor(Math.random() * 1000) + 2000); // 2 ~ 3초 랜덤
+    } else if (state === "ready") {
+      // 성급하게 클릭
+      clearTimeout(timeout.current);
+      setState('waiting');
+      setMessage('초록색이 된 후에 클릭하세요.');
+    } else if (state === "now") {
+      // 반응속도 체크
+      endTime.current = new Date();
+      setState('waiting');
+      setMessage('클릭해서 시작하세요!');
+      setResult((prevResult) => {
+        return [...prevResult, endTime.current - startTime.current];
+      })
+    }
+  }
+
+  const onReset = () => {
+    setResult([]);
+  }
+
+  const renderAverage = () => {
+    return result.length === 0 
+      ? null 
+      : (
+      <>
+        <div>
+          평균 시간:{" "}
+          {result.reduce((a, c) => a + c) / result.length}
+        </div>
+        <button onClick={onReset}>초기화</button>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div
+        id="screen"
+        className={state}
+        onClick={onClickScreen}
+      >
+        {message}
+      </div>
+      {renderAverage()}
+    </>
+  );
+
+};
+
+export default ResponseCheck;
