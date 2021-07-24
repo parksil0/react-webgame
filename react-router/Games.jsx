5c6321c9774c574d84961ebf80fb959388a17d1b
parksil0 <parksil00@naver.com>
2021-07-24 20:04:59 +0900

    react-router-dom 을이용하여 url 설정

---
+++ react-router/Games.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import GameMatcher from './GameMatcher';
+
+const Games = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Link to="/game/number-baseball">숫자야구</Link>
+        &nbsp;
+        <Link to="/game/rock-scissors-paper">가위바위보</Link>
+        &nbsp;
+        <Link to="/game/lotto-generator">로또생성기</Link>
+        &nbsp;
+        <Link to="/game/index">게임 매쳐</Link>
+      </div>
+      <div>
+        <Route path="/game/:name" component={GameMatcher} />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default Games;
